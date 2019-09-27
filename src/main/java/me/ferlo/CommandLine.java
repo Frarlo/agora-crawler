a64e2ae30aee1920de42390860e6470da14bdf22
Frarlo <ferlolovesnewmail@gmail.com>
2019-09-27 23:17:37 +0200

    Made CommandLine class ready for parallel execution

--- src/main/java/me/ferlo/CommandLine.java
+++ src/main/java/me/ferlo/CommandLine.java
@@ -8,6 +8,7 @@ import me.ferlo.crawler.course.Course;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class CommandLine {
@@ -59,24 +60,24 @@ public class CommandLine {
         if(!dest.exists() && !dest.mkdirs())
             throw new AssertionError("Couldn't create dirs " + dest);
 
-        int[] n = { 0 };
+        final AtomicInteger n = new AtomicInteger(0);
         IntStream.range(0, crawledCourses.size())
                 .filter(selected::contains)
                 .mapToObj(i -> crawledCourses.get(i - 1))
                 .forEach(course -> {
-                    File courseDir = new File(dest, (++n[0]) + ". " + course.getTitle().replaceAll("[\\\\/:*?\"<>|]", "").trim());
+                    File courseDir = new File(dest, n.incrementAndGet() + ". " + course.getTitle().replaceAll("[\\\\/:*?\"<>|]", "").trim());
 
                     if(!courseDir.exists())
                         System.out.println("Creating dir " + courseDir.getAbsolutePath());
                     if(!courseDir.exists() && !courseDir.mkdirs())
                         throw new AssertionError("Couldn't create dirs " + courseDir);
 
-                    int[] categoriesN = { 0 };
+                    final AtomicInteger categoriesN = new AtomicInteger(0);
 
                     List<Category> categories = crawler.fetchCategories(course.getHref());
                     categories.forEach(category -> {
 
-                        File categoryDir = new File(courseDir, (++categoriesN[0]) + ". " + category.getName().replaceAll("[\\\\/:*?\"<>|]", "").trim());
+                        File categoryDir = new File(courseDir, categoriesN.incrementAndGet() + ". " + category.getName().replaceAll("[\\\\/:*?\"<>|]", "").trim());
 
                         if(!categoryDir.exists())
                             System.out.println("Creating dir " + categoryDir.getAbsolutePath());
@@ -84,15 +85,15 @@ public class CommandLine {
                             throw new AssertionError("Couldn't create dirs " + categoryDir);
 
                         LinkedList<Integer> currentNumbers = new LinkedList<>();
-                        int[] lastIndent = { -1 };
+                        final AtomicInteger lastIndent = new AtomicInteger(-1);
 
                         category.getActivities().forEach(activity -> {
 
-                            if(activity.getIndent() > lastIndent[0])
+                            if(activity.getIndent() > lastIndent.get())
                                 currentNumbers.add(0);
-                            if(activity.getIndent() < lastIndent[0])
+                            if(activity.getIndent() < lastIndent.get())
                                 currentNumbers.removeLast();
-                            lastIndent[0] = activity.getIndent();
+                            lastIndent.set(activity.getIndent());
 
                             currentNumbers.set(currentNumbers.size() - 1, currentNumbers.getLast() + 1);
 
